7ed0f05b2f369b71c5f93002455a992484ff8419
Simon Gao <simon.shubin@gmail.com>
2023-10-10 16:38:21 +0200

    added CORS

--- src/main/java/tradearea/warehouse/WarehouseController.java
+++ src/main/java/tradearea/warehouse/WarehouseController.java
@@ -2,6 +2,7 @@ package tradearea.warehouse;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.MediaType;
 import tradearea.model.WarehouseData;
 
 @RestController
+@CrossOrigin
 public class WarehouseController {
 
     @Autowired
