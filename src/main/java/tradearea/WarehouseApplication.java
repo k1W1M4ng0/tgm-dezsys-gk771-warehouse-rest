8d0dbcc731e984a705112b3fe1c1834ad2cb74a0
Simon Gao <simon.shubin@gmail.com>
2023-11-21 13:53:56 +0100

    changed port to be set by args

--- src/main/java/tradearea/WarehouseApplication.java
+++ src/main/java/tradearea/WarehouseApplication.java
@@ -1,12 +1,28 @@
 package tradearea;
 
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WarehouseApplication {
+    private static final Logger logger = LoggerFactory.getLogger(WarehouseApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(WarehouseApplication.class, args);
+        if(args.length > 0) {
+            logger.info("Running on Port " + args[0]);
+            SpringApplication app = new SpringApplication(WarehouseApplication.class);
+            app.setDefaultProperties(
+                Collections.singletonMap("server.port", Integer.parseInt( args[0]))
+            );
+            app.run();
+        }
+        else {
+            logger.info("no args entered, using port 8080");
+            SpringApplication.run(WarehouseApplication.class);
+        }
 	}
 }
